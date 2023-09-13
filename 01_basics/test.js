ca86dfacf68d6513c4a9766c54924d575fdf60fa
Winter <119809181+rahul-2327@users.noreply.github.com>
2023-09-13 17:29:21 +0000

    just how to declare the variables

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
-console.log("rahul")
+const accountId = 144553
+let accountEmail = "rahul@google.com"
+var accountPass = 12345
+
+accountCity = "Patna"
+
+console.table([accountId,accountEmail,accountPass])

5c5c5fc82eb7bd849791fc7bd82c69fc8ef3f253
Winter <119809181+rahul-2327@users.noreply.github.com>
2023-09-16 17:35:44 +0000

    conversion_of_datatypes

---
+++ 01_basics/conversion_operations.js
@@ -0,0 +1,14 @@
+// let score = null
+// let id = undefined
+// let gin = Number(id)
+// let num = Number(score)
+// console.log(typeof num)
+// console.log(typeof score)
+// console.log(num)
+// console.log(typeof gin)
+// console.log(gin)
+
+//let isLoggedIn = 1
+let isLoggedIn = ""// --> false
+let isBooleanLoggedIn = Boolean(isLoggedIn)
+console.log(isBooleanLoggedIn)

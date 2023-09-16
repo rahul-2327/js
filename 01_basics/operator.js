76e97ac0bcd3f850a80c2d64ed5939716d1c4d1f
Winter <119809181+rahul-2327@users.noreply.github.com>
2023-09-16 18:50:18 +0000

    all_about_operators

---
+++ 01_basics/operator.js
@@ -0,0 +1,15 @@
+// let str1 = "rahul "
+// let str2 = "kumar"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+"2"+2);
+// console.log(1+2+"2");
+// console.log(1+2+"2"+2);
+// console.log(1+2+"2"+"2");
+
+let health = 100;
+health++;
+console.log(health)
